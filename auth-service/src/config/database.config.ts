c047ea44cff5a79b1f98566012d21e7b634bc4ec
Jobayer Ahmed <Jobayer6735@gmail.com>
2024-08-29 08:36:43 +0600

    Implementing the auth-service database connection

---
+++ auth-service/src/config/database.config.ts
@@ -0,0 +1,25 @@
+export default {
+  client: 'pg',
+  connection: {
+    host: process.env.DATABASE_CONNECTION_HOST,
+    port: +process.env.DATABASE_CONNECTION_PORT,
+    database: process.env.DBNAME,
+    user: process.env.DATABASE_CONNECTION_USERNAME,
+    password: process.env.DATABASE_CONNECTION_PASSWORD,
+  },
+  pool: {
+    min: 10,
+    max: 30,
+    idleTimeoutMillis: 60000,
+  },
+  acquireConnectionTimeout: 20000,
+  comigrations: {
+    enabled: +process.env.DATABASE_MIGRATION_ENABLED > 0,
+    tableName: process.env.DATABSE_MIGRATION_TABLE_NAME,
+    directory: process.env.DATABASE_MIGRATION_DIR,
+  },
+  seeds: {
+    enabled: +process.env.DATABASE_SEED_ENABLED > 0,
+    directory: process.env.DATABASE_SEED_DIR,
+  },
+};
