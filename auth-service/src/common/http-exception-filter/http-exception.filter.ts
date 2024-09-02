9da5908294ee1f0f152e442042c78653464bf769
Jobayer Ahmed <Jobayer6735@gmail.com>
2024-09-02 23:53:58 +0600

    Implemented the error handling feature

---
+++ auth-service/src/common/http-exception-filter/http-exception.filter.ts
@@ -0,0 +1,28 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+
+    let errorMessage = (<any>exception.getResponse()).message;
+
+    if (errorMessage && Array.isArray(errorMessage)) {
+      errorMessage = errorMessage.join(', ');
+    }
+
+    response.status(status).json({
+      success: false,
+      message: errorMessage,
+      result: null,
+    });
+  }
+}
