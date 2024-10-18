d2352d83b3f2315d7df1cd75b58ac801fac5bcd8
Jobayer Ahmed <Jobayer6735@gmail.com>
2024-10-19 01:27:31 +0600

    Added default user seed file

---
+++ auth-service/src/migrations/20241018162251_seed_user.ts
@@ -0,0 +1,16 @@
+import type { Knex } from 'knex';
+import * as bcrypt from 'bcryptjs';
+
+const payload: Record<string, string | boolean> = {
+  first_name: 'John',
+  last_name: 'Doe',
+  email: 'john.doe@example.com',
+  password: bcrypt.hashSync('v0N6OI8'),
+  is_verified: true,
+};
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.insert(payload).into('users');
+}
+
+export async function down(knex: Knex): Promise<void> {}
